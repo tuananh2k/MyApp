3af5620957ba7fa7b231a45e35e2ac0f417abed0
tuananh2k <tuananh2k@users.noreply.github.com>
2020-11-25 09:13:00 +0700

    25/11

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatefulWidget{
   String name;
   int age;
   MyApp({this.name, this.age});
 
   @override
-  Widget build(BuildContext context){
-    return  MaterialApp(
-      title: 'Hello $name',
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp>{
+  String _email = '';
+  final emailEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'This is my first code',
       home: Scaffold(
-        body: Center(child: Text('Hello $name - age: $age ',
-            style: TextStyle(fontSize: 30),
-            textDirection: TextDirection.ltr
-        )),
-      ),
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center, //hien thi o trung tam man hinh
+           children: <Widget> [
+             Container(
+               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), //horizontal: can vi tri left + right, vertical: can vi tri top + bottom
+               child: TextField(
+                 controller: emailEditingController,
+                 onChanged: (text){
+                   this.setState(() {
+                     _email = text;
+                   });
+                 },
+                 decoration: InputDecoration(
+                     border: OutlineInputBorder(
+                       borderRadius: const BorderRadius.all( // bo tron goc o textfield
+                         const Radius.circular(20)
+                       )
+                     ),
+                     labelText: 'Enter your name:'
+                 ),
+               ),
+             ),
+             Text('See result:',
+             style: TextStyle(fontSize: 30, color: Colors.blue),
+             ),
+             Text('Hello $_email',
+             style: TextStyle(fontSize: 30, color: Colors.red),
+             )
+           ],
+         ),
+        ),
+      )
     );
   }
+
 }
